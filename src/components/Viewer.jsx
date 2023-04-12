441dc8f08acae3bcad25b533857d98df5ad15d0e
Marin Cholakov <marinvch@gmail.com>
2023-04-12 09:39:17 +0300

    update: cleaning unneeded stuff

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import HTMLFlipBook from "react-pageflip";
+import Page8 from '../Assets/newspaper/page8.jpg'
 
 function Newspapper() {
+  const pages = [Page8]
   return (
     <HTMLFlipBook width={300} height={500}>
-      <div className="demoPage">Page 1</div>
-      <div className="demoPage">Page 2</div>
-      <div className="demoPage">Page 3</div>
-      <div className="demoPage">Page 4</div>
+      {pages.map((page) => {
+        return <div><img src={page} alt={'page 8'}/></div>
+      })}
+
     </HTMLFlipBook>
   );
 }
---
+++ src/pages/Newspapper.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Viewer from "../Components/Viewer"
+
+const Newspapper = () => {
+    return (
+        <Viewer />
+    )
+}
+
+export default Newspapper
